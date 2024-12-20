a173a883893e31f232370a558ecc981be8e1c55b
MatheusDelgado <a31542@alunos.ipca.pt>
2024-12-20 14:19:26 +0000

    third commit

--- trabalhoPI.c
+++ trabalhoPI.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_FUNCIONARIOS 50
 #define MAX_REFEICOES 100
@@ -15,14 +16,15 @@ typedef struct {
 
 typedef struct {
     char dia[10];
-    char prato_peixe[50];
+    char data[20];
+    char prato_peixe[100];
     int calorias_peixe;
-    char prato_carne[50];
+    char prato_carne[100];
     int calorias_carne;
-    char prato_dieta[50];
+    char prato_dieta[100];
     int calorias_dieta;
-    char prato_vegetariano[50];
-    int calorias_vegetariano;
+    char prato_vegan[100];
+    int calorias_vegan;
 } Refeicao;
 
 typedef struct {
@@ -37,11 +39,12 @@ int carregarEscolhas(const char *ficheiro, Escolha escolhas[]);
 void mostrarRefeicoesDia(const char *dia, Funcionario funcionarios[], int numFuncionarios, 
                          Escolha escolhas[], int numEscolhas, Refeicao refeicoes[], int numRefeicoes);
 void listarUtentesDespesas(Funcionario funcionarios[], int numFuncionarios, Escolha escolhas[], int numEscolhas);
-int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[], int maxFuncionarios);
+int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[]);
 int mostrarEmenta(const char *ficheiro, Refeicao refeicoes[]);
 void removerFuncionario(const char *ficheiro);
 void registarEscolha(const char *ficheiro, Escolha escolhas[], int *numEscolhas, int maxEscolhas);
-void editarFuncionario(const char *ficheiro, Funcionario funcionarios[], int numFuncionarios);
+void editarFuncionario(const char *ficheiro, Funcionario funcionarios[]);
+void mostrarFuncionarios(const char *ficheiro, Funcionario funcionarios[]);
 
 int main() {
     Funcionario funcionarios[MAX_FUNCIONARIOS];
@@ -59,10 +62,10 @@ int main() {
     char opcao;
     do {
         printf("\nMenu:\n");
-        printf("1 - Carregar funcionarios\n");
+        printf("1 - Mostrar todos os funcionarios registrados\n");
         printf("2 - Inserir novo funcionario\n");
         printf("3 - Mostrar ementa semanal\n");
-        printf("4 - Carregar escolhas\n");
+        printf("4 - Mostrar escolhas dos funcionarios\n");
         printf("5 - Mostrar refeicoes de um dia\n");
         printf("6 - Listar utentes e despesas\n");
         printf("7 - Remover funcionario\n");
@@ -71,15 +74,15 @@ int main() {
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
         scanf(" %c", &opcao);
+        getchar();
 
         switch (opcao) {
             case '1':
-                numFuncionarios = carregarFuncionarios(ficheiroFuncionarios, funcionarios);
-                printf("%d funcionarios carregados com sucesso.\n", numFuncionarios);
+                mostrarFuncionarios(ficheiroFuncionarios, funcionarios);
                 break;
 
             case '2':
-                if (inserirFuncionario(ficheiroFuncionarios, funcionarios, MAX_FUNCIONARIOS) == 0) {
+                if (inserirFuncionario(ficheiroFuncionarios, funcionarios) == 0) {
                     numFuncionarios++;
                 }
                 break;
@@ -89,11 +92,11 @@ int main() {
                 if (numRefeicoes > 0) {
                     printf("Ementa semanal:\n");
                     for (int i = 0; i < numRefeicoes; i++) {
-                        printf("%s: Peixe: %s (%d cal), Carne: %s (%d cal), Dieta: %s (%d cal), Vegetariano: %s (%d cal)\n",
-                               refeicoes[i].dia, refeicoes[i].prato_peixe, refeicoes[i].calorias_peixe,
+                        printf("%s, %s: Peixe: %s (%d cal), Carne: %s (%d cal), Dieta: %s (%d cal), Vegan: %s (%d cal)\n",
+                               refeicoes[i].dia, refeicoes[i].data, refeicoes[i].prato_peixe, refeicoes[i].calorias_peixe,
                                refeicoes[i].prato_carne, refeicoes[i].calorias_carne,
                                refeicoes[i].prato_dieta, refeicoes[i].calorias_dieta,
-                               refeicoes[i].prato_vegetariano, refeicoes[i].calorias_vegetariano);
+                               refeicoes[i].prato_vegan, refeicoes[i].calorias_vegan);
                     }
                 } else {
                     printf("Nenhuma ementa encontrada.\n");
@@ -107,8 +110,9 @@ int main() {
 
             case '5': {
                 char dia[10];
-                printf("Digite o dia (e.g., Segunda): ");
+                printf("Digite o dia (ex: Segunda): ");
                 scanf(" %s", dia);
+                getchar();
                 mostrarRefeicoesDia(dia, funcionarios, numFuncionarios, escolhas, numEscolhas, refeicoes, numRefeicoes);
                 break;
             }
@@ -126,7 +130,7 @@ int main() {
                 break;
 
             case '9':
-                editarFuncionario(ficheiroFuncionarios, funcionarios, numFuncionarios);
+                editarFuncionario(ficheiroFuncionarios, funcionarios);
                 break;
 
             case '0':
@@ -183,19 +187,53 @@ void removerFuncionario(const char *ficheiro) {
 
 void registarEscolha(const char *ficheiro, Escolha escolhas[], int *numEscolhas, int maxEscolhas) {
     if (*numEscolhas >= maxEscolhas) {
-        printf("Limite maximo de escolhas atingido.\n");
+        printf("\nLimite maximo de escolhas atingido.\n");
         return;
     }
 
     Escolha novaEscolha;
-    printf("Digite o dia (e.g., Segunda): ");
-    scanf(" %s", novaEscolha.dia);
-    printf("Digite o numero do funcionario: ");
+    const char *diasValidos[] = {"2feira", "3feira", "4feira", "5feira", "6feira"};
+    int diaValido = 0;
+    do { //ciclo para garantir que o utente escreva um dia da semana valido
+        printf("\nDigite o dia (2feira, 3feira, 4feira, 5feira, 6feira): \n");
+        scanf(" %s", novaEscolha.dia);
+
+        diaValido = 0;
+        for (int i = 0; i < 5; i++) {
+            if (strcmp(novaEscolha.dia, diasValidos[i]) == 0) {
+                diaValido = 1;
+                break;
+            }
+        }
+
+        if (!diaValido) {
+            printf("\nDia invalido. Por favor, insira um dia valido.\n");
+        }
+    } while (!diaValido);
+
+   do { //garante que vai digitar um numero de funcionario valido
+    printf("Digite o numero do funcionario (4 digitos, 0000 a 9999): ");
     scanf(" %s", novaEscolha.numero_funcionario);
-    printf("Digite o tipo de prato (C - Carne, P - Peixe, D - Dieta, V - Vegetariano): ");
+
+    if (strlen(novaEscolha.numero_funcionario) == 4 && 
+        strspn(novaEscolha.numero_funcionario, "0123456789") == 4) { //strspn vai verificar se os caracteres sao digitos de 0 a 9
+        break;
+    }
+    printf("Numero invalido. Por favor, insira um numero valido (4 digitos).\n");
+} while (1);
+
+do { //garante que vai escolher uma opcao valida
+    printf("Digite o tipo de prato (C - Carne, P - Peixe, D - Dieta, V - Vegean): ");
     scanf(" %s", novaEscolha.tipo_prato);
 
-    escolhas[*numEscolhas] = novaEscolha;
+    if (strlen(novaEscolha.tipo_prato) == 1 && 
+        strchr("CPDV", toupper(novaEscolha.tipo_prato[0]))) { //strchr verifica se o caracter esta em "CPDV". toupper torna minuscula em maiuscula
+        break;
+    }
+    printf("Tipo de prato invalido. Por favor, escolha C, P, D ou V.\n");
+} while (1);
+
+    escolhas[*numEscolhas] = novaEscolha; //para guardar a escolha do utente
     (*numEscolhas)++;
 
     FILE *file = fopen(ficheiro, "a");
@@ -208,6 +246,24 @@ void registarEscolha(const char *ficheiro, Escolha escolhas[], int *numEscolhas,
     fclose(file);
 
     printf("Escolha registrada com sucesso.\n");
+}
+
+void mostrarFuncionarios(const char *ficheiro, Funcionario funcionarios[]) {
+    int numFuncionarios = carregarFuncionarios(ficheiro, funcionarios);
+    if (numFuncionarios == 0) {
+        printf("Nenhum funcionário registrado.\n");
+        return;
+    }
+
+    printf("\nLista de funcionarios registrados:\n");
+    printf("Numero de funcionario | Nome  | NIF  | Telefone\n");
+    for (int i = 0; i < numFuncionarios; i++) {
+        printf("%s | %s | %s | %s\n", 
+               funcionarios[i].numero, 
+               funcionarios[i].nome, 
+               funcionarios[i].nif, 
+               funcionarios[i].telefone);
+    }
 }
 
 int carregarFuncionarios(const char *ficheiro, Funcionario funcionarios[]) {
@@ -232,8 +288,12 @@ int carregarFuncionarios(const char *ficheiro, Funcionario funcionarios[]) {
 }
 
 
-int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[], int maxFuncionarios) {
+int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[]) {
     int nrFuncionario = carregarFuncionarios(ficheiro, funcionarios);
+    if (nrFuncionario >= MAX_FUNCIONARIOS) {
+        printf("Limite maximo de funcionarios atingido.\n");
+        return -1;
+    }
     if(nrFuncionario < 0){
         printf("\n Numero invalido! Tente novamente, por favor.");
         return -1;
@@ -243,7 +303,7 @@ int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[], int max
     char nrFuncionarioStr[5];  // Mudando de int para string
     scanf(" %s", nrFuncionarioStr);
 
-    for (int i = 0; i < nrFuncionario; i++) {  // Verifica se o número já existe
+    for (int i = 0; i < nrFuncionario; i++) { 
         if (strcmp(funcionarios[i].numero, nrFuncionarioStr) == 0) {  // Comparando como string
             printf("Numero de funcionario ja registado!\n");
             return -1;
@@ -251,7 +311,7 @@ int inserirFuncionario(const char *ficheiro, Funcionario funcionarios[], int max
     }
 
     printf("Introduza o nome do funcionario:\n");
-    scanf(" %156[^\n]", funcionarios[nrFuncionario].nome);
+    scanf(" %156[^\n]", funcionarios[nrFuncionario].nome); //%156[^\n] permite um nome ate 156 caracteres e le tudo ate a quebra de linha
     printf("Introduza o NIF do funcionario:\n");
     scanf(" %s", funcionarios[nrFuncionario].nif);
     printf("Introduza o telefone do funcionario:\n");
@@ -287,20 +347,27 @@ int carregarEmenta(const char *ficheiro, Refeicao refeicoes[]) {
         return 0;
     }
 
-    char linha[200];
+    char linha[300];
     int count = 0;
+
     while (fgets(linha, sizeof(linha), file)) {
-        sscanf(linha, "%[^;];%[^;];%d;%[^;];%d;%[^;];%d;%[^;];%d",
-               refeicoes[count].dia, 
-               refeicoes[count].prato_peixe, &refeicoes[count].calorias_peixe,
-               refeicoes[count].prato_carne, &refeicoes[count].calorias_carne,
-               refeicoes[count].prato_dieta, &refeicoes[count].calorias_dieta,
-               refeicoes[count].prato_vegetariano, &refeicoes[count].calorias_vegetariano);
-        count++;
+        if (sscanf(linha, "%[^;];%[^;];%[^;];%d;%[^;];%d;%[^;];%d;%[^;];%d",
+                   refeicoes[count].dia,
+                   refeicoes[count].data,
+                   refeicoes[count].prato_peixe, &refeicoes[count].calorias_peixe,
+                   refeicoes[count].prato_carne, &refeicoes[count].calorias_carne,
+                   refeicoes[count].prato_dieta, &refeicoes[count].calorias_dieta,
+                   refeicoes[count].prato_vegan, &refeicoes[count].calorias_vegan) == 10) {
+            count++;
+        } else {
+            printf("Erro ao interpretar a linha: %s\n", linha);
+        }
     }
+
     fclose(file);
     return count;
 }
+
 
 int mostrarEmenta(const char *ficheiro, Refeicao refeicoes[]){
     FILE *file = fopen(ficheiro, "r");
@@ -312,11 +379,11 @@ int mostrarEmenta(const char *ficheiro, Refeicao refeicoes[]){
     int count = 0;
     while (fgets(linha, sizeof(linha), file)){
         sscanf(linha, "%[^;]; %[^;]; %[^;]; %d; %[^;]; %d; %[^;]; %d; %[^;]; %d",
-               refeicoes[count].dia,
+               refeicoes[count].dia, refeicoes[count].data,
                refeicoes[count].prato_peixe, &refeicoes[count].calorias_peixe,
                refeicoes[count].prato_carne, &refeicoes[count].calorias_carne,
                refeicoes[count].prato_dieta, &refeicoes[count].calorias_dieta,
-               refeicoes[count].prato_vegetariano, &refeicoes[count].calorias_vegetariano);
+               refeicoes[count].prato_vegan, &refeicoes[count].calorias_vegan);
         count++;
     }
     fclose(file);
@@ -346,7 +413,7 @@ int carregarEscolhas(const char *ficheiro, Escolha escolhas[]) {
 
 void mostrarRefeicoesDia(const char *dia, Funcionario funcionarios[], int numFuncionarios, 
                          Escolha escolhas[], int numEscolhas, Refeicao refeicoes[], int numRefeicoes) {
-    printf("Refeições para o dia %s:\n", dia);
+    printf("Refeicoes do dia %s:\n", dia);
 
     for (int i = 0; i < numEscolhas; i++) {
         if (strcmp(escolhas[i].dia, dia) == 0) {
@@ -366,7 +433,7 @@ void mostrarRefeicoesDia(const char *dia, Funcionario funcionarios[], int numFun
                     if (strcmp(escolhas[i].tipo_prato, "P") == 0) strcpy(prato, refeicoes[k].prato_peixe);
                     else if (strcmp(escolhas[i].tipo_prato, "C") == 0) strcpy(prato, refeicoes[k].prato_carne);
                     else if (strcmp(escolhas[i].tipo_prato, "D") == 0) strcpy(prato, refeicoes[k].prato_dieta);
-                    else if (strcmp(escolhas[i].tipo_prato, "V") == 0) strcpy(prato, refeicoes[k].prato_vegetariano);
+                    else if (strcmp(escolhas[i].tipo_prato, "V") == 0) strcpy(prato, refeicoes[k].prato_vegan);
                 }
             }
 
@@ -398,7 +465,8 @@ void listarUtentesDespesas(Funcionario funcionarios[], int numFuncionarios, Esco
                refeicoesServidas[i], refeicoesServidas[i] * precoPorRefeicao);
     }
 }
-void editarFuncionario(const char *ficheiro, Funcionario funcionarios[], int numFuncionarios) {
+void editarFuncionario(const char *ficheiro, Funcionario funcionarios[]) {
+    int numFuncionarios = carregarFuncionarios(ficheiro, funcionarios);
     char numero[5];
     printf("Digite o numero do funcionario a ser editado: ");
     scanf(" %s", numero);
@@ -426,7 +494,8 @@ void editarFuncionario(const char *ficheiro, Funcionario funcionarios[], int num
     getchar();
     fgets(novoNome, sizeof(novoNome), stdin);
     if (novoNome[0] != '\n') {
-        novoNome[strcspn(novoNome, "\n")] = '\0';
+        novoNome[strcspn(novoNome, "\n")] = '\0'; // percorre a str1 e verifica se algum caracter pertence a str 2 e se pertencer vai retornar o indice
+        // - se nenhum pertencer, retorna str1
         strcpy(funcionarios[indice].nome, novoNome);
     }
 
